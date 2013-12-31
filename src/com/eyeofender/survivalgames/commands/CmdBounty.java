a1f766b9d61b89dae673d144e030740851744c4a
Avery246813579 <avery246813579@gmail.com>
2013-12-31 12:51:50 -0500

    Fixed bounty bug!

--- src/com/eyeofender/survivalgames/commands/CmdBounty.java
+++ src/com/eyeofender/survivalgames/commands/CmdBounty.java
@@ -59,6 +59,10 @@ public class CmdBounty implements CommandExecutor{
 					return true;
 				}
 				
+				if(amount <= 50){
+					plugin.sendMessage(player, ChatColor.RED + "You have to bounty a least 50 credits.")
+				}
+				
 				if(targerPlayer == player){
 					plugin.sendMessage(player, "You can not set a bounty on yourself.");
 					return true;
